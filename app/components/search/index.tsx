30c634cde08eae705d99cec33727f0d3a95db967
Daniel da Silva <danielfdsilva@gmail.com>
2024-12-10 12:54:44 +0000

    Add basic search page select

--- app/components/search/index.tsx
+++ app/components/search/index.tsx
@@ -1,13 +1,100 @@
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { useLocation } from 'wouter';
+import { Badge, Box, Heading } from '@chakra-ui/react';
+import { useQuery } from '@tanstack/react-query';
+import { Select } from 'chakra-react-select';
+
+import { PanelHeader } from '$components/common/panel-header';
+import { getJsonFn, IndividualListed } from '$utils/api';
+import { RouteErrorHandler } from '$components/common/error';
+
+interface SelectOption {
+  label: string;
+  value: string;
+}
+
+interface SelectOptionGroup {
+  label: string;
+  options: SelectOption[];
+}
+
+const formatGroupLabel = (data) => (
+  <Heading
+    size='xs'
+    color='base.400'
+    display='flex'
+    justifyContent='space-between'
+    gap={4}
+  >
+    {data.label}
+    <Badge bg='base.400a' color='surface.500' px={2} borderRadius='sm'>
+      {data?.options.length.toString().padStart(2, '0')}
+    </Badge>
+  </Heading>
+);
 
 /**
  * Search Page component.
  */
 export default function Component() {
+  const [, navigate] = useLocation();
+
+  const { data, error } = useQuery<
+    IndividualListed[],
+    Error,
+    SelectOptionGroup[]
+  >({
+    queryKey: ['individuals'],
+    queryFn: getJsonFn(`/api/individual/index.json`),
+    select(data) {
+      const groups = data.reduce<Record<string, IndividualListed[]>>(
+        (acc, individual) => {
+          const species = individual.species || 'Unknown';
+
+          return {
+            ...acc,
+            [species]: [...(acc[species] || []), individual]
+          };
+        },
+        {}
+      );
+
+      return Object.entries(groups).map(([species, individuals]) => ({
+        label: species,
+        options: individuals.map((individual) => ({
+          label: individual.id,
+          value: individual.id
+        }))
+      }));
+    }
+  });
+
+  if (error) {
+    return <RouteErrorHandler error={error} />;
+  }
+
   return (
-    <Box>
-      This is search
+    <Box w='100%'>
+      <PanelHeader
+        suptitle='Explore'
+        heading='Individuals'
+        borderBottom='1px'
+        borderBottomColor='base.100a'
+      />
+      <Box mt={4}>
+        {data && (
+          <Select
+            options={data}
+            size='sm'
+            selectedOptionColorScheme='primary'
+            focusBorderColor='primary.500'
+            formatGroupLabel={formatGroupLabel}
+            onChange={(option: SelectOption) => {
+              option && navigate(`/individual/${option.value}`);
+            }}
+          />
+        )}
+      </Box>
     </Box>
   );
 }
