ed332a550ac3dd10cee888d020e8f70c9b01f1c5
Daniel da Silva <danielfdsilva@gmail.com>
2024-12-17 16:38:51 +0000

    Add deploy script

--- app/main.tsx
+++ app/main.tsx
@@ -2,10 +2,13 @@ import React, { useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Router } from 'wouter';
 
 import theme from '$styles/theme';
 
 import PageLayout from '$components/common/page-layout';
+
+const publicUrl = process.env.PUBLIC_URL || '';
 
 // Create a client
 const queryClient = new QueryClient({
@@ -30,7 +33,17 @@ function Root() {
   return (
     <ChakraProvider theme={theme} resetCSS>
       <QueryClientProvider client={queryClient}>
-        <PageLayout />
+        <Router
+          base={
+            new URL(
+              publicUrl.startsWith('http')
+                ? publicUrl
+                : `https://ds.io/${publicUrl.replace(/^\//, '')}`
+            ).pathname
+          }
+        >
+          <PageLayout />
+        </Router>
       </QueryClientProvider>
     </ChakraProvider>
   );
